67418c779f8f64c594874bba373b64dc0e27d42e
LauSnchz <laurence.sanchez@benilde.edu.ph>
2022-11-07 23:34:21 +0800

    Added Tutorial Sequence and Sprites. Fixed some assets in Main Menu scene
    To fix tutorial text and sequence

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,30 +2,59 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Tutorial;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
     private UserInputSequence us;
-
+    
     [SerializeField] private ToolTipAdapter _adapter;
+    
+    /*
+     * 1. Introduction
+     * 2. Visitor Requests (Tooltip)
+     * 3. Visitor Patience Meter
+     * 4. Book Mechanics and Controls
+     * 5. Scoring (Tooltip)
+     */
 
     private void Awake()
     {
         var controller = GetComponent<SequenceController>();
         
         us = new UserInputSequence(controller);
-
+        
         controller.AddSequence(new WaitSequence(controller, 5.0f))
-            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(136, -74f), 10, GetComponent<RectTransform>()))
-            .AddSequence(new ToolTipSequence(controller, _adapter, "test"))
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(254, 191f), 5, GetComponent<RectTransform>())) // Visitor Request
+            .AddSequence(new WaitSequence(controller, 1.0f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Visitor Request Info"))
             .AddSequence(new WaitSequence(controller, 2.0f))
-            .AddSequence(new ToolTipSequence(controller, _adapter, "no"))
             .AddSequence(us)
             .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
-            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(136, -74f), 10, GetComponent<RectTransform>()))
-            .AddSequence(us)
+            
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(-136, 270f), 5, GetComponent<RectTransform>())) // Patience Meter
             .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Patience Meter Info"))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
+            
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(38, -603f), 5, GetComponent<RectTransform>())) // Book Mechanics
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Book Mechanics Info"))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
+            
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(397, 706f), 5, GetComponent<RectTransform>())) // Scoring
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Scoring Info"))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            
+            // end tutorial, go to game mode select scene
+            
             .AddSequence(new CustomSequence(controller)
                 .SetAction((s, o) =>
                 {
