3a8906689f874c40f4b3926d6c135a58f85f7034
LauSnchz <laurence.sanchez@benilde.edu.ph>
2022-11-08 16:46:45 +0800

    Added the proper tutorial description
    To add:
    -Tutorial book text
    -book 'validation'
    -game mode to tutorial scene

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -10,6 +10,7 @@ public class Hand : MonoBehaviour
     private UserInputSequence us;
     
     [SerializeField] private ToolTipAdapter _adapter;
+    [SerializeField] private Reader Chichay;
     
     /*
      * 1. Introduction
@@ -26,29 +27,54 @@ public class Hand : MonoBehaviour
         us = new UserInputSequence(controller);
         var rectTransform = GetComponent<RectTransform>();
         
-        controller.AddSequence(new WaitSequence(controller, 5.0f))
-            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(254, 191f), 5, rectTransform)) // Visitor Request
-            .AddSequence(new WaitSequence(controller, 1.0f))
-            .AddSequence(new ToolTipSequence(controller, _adapter, "Visitor Request Info"))
+        controller.AddSequence(new WaitSequence(controller, 3.0f))
+            // Visitor Request
+            .AddSequence(new CustomSequence(controller, (s, o) =>
+            {
+                Chichay.ShowHideRequest(true);
+                s.SetStatus(true);
+            }))
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(254, 191f), 5, rectTransform)) 
+            .AddSequence(new WaitSequence(controller, 1.5f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Ito ang hinihingi ng bisita, konektado ito sa librong gusto niya. \n \n This is the visitor's request, this picture is the book that the visitor wants."))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new WaitSequence(controller, 1.5f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
+
+            // Patience Meter
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(-136, 270f), 5, rectTransform)) 
+            .AddSequence(new WaitSequence(controller, 1.5f))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Ito ang 'Patience Meter'. Ibigay ang librong hinihingi bago maubos ang Patience Meter. \n \n This is the 'Patience Meter'. Give the right book before the patience meter runs out."))
             .AddSequence(new WaitSequence(controller, 2.0f))
             .AddSequence(us)
             .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
             
-            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(-136, 270f), 5, rectTransform)) // Patience Meter
+            // Book Mechanics
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(38, -603f), 5, rectTransform)) 
             .AddSequence(new WaitSequence(controller, 2.0f))
-            .AddSequence(new ToolTipSequence(controller, _adapter, "Patience Meter Info"))
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Pindutin ang libro para makita ang deskripsyon. \n \n When a book is tapped, its description will appear."))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Pindutin ang naka-highlight upang makita ang deskripsyon ng salita. \n \n Tap the highlighted word to show its description."))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Pindutin ang labas libro upang isara ito. \n \n Tap outside of the book to close it."))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Pindutin ng dalawang beses ang libro para piliin ito. \n \n Double tap to select the book."))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Pindutin ang 'Confirm' upang ibigay ang libro sa bisita. \n \n Tap 'Confirm' to give the book to the visitor."))
+            .AddSequence(new WaitSequence(controller, 2.0f))
+            .AddSequence(us)
+            .AddSequence(new ToolTipSequence(controller, _adapter, "Pindutin ang labas ng libro para ikansela ang napili. \n \n Tap outside the book to cancel."))
             .AddSequence(new WaitSequence(controller, 2.0f))
             .AddSequence(us)
             .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
-            
-            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(38, -603f), 5, rectTransform)) // Book Mechanics
-            .AddSequence(new WaitSequence(controller, 2.0f))
-            .AddSequence(new ToolTipSequence(controller, _adapter, "Book Mechanics Info"))
-            .AddSequence(new WaitSequence(controller, 2.0f))
-            .AddSequence(us)
-            .AddSequence(new ToolTipSequence(controller, _adapter, "", false))
-            
-            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(397, 706f), 5, rectTransform)) // Scoring
+
+            // Scoring
+            .AddSequence(new MoveSequenceCanvas(controller, new Vector2(397, 706f), 5, rectTransform)) 
             .AddSequence(new WaitSequence(controller, 2.0f))
             .AddSequence(new ToolTipSequence(controller, _adapter, "Scoring Info"))
             .AddSequence(new WaitSequence(controller, 2.0f))
