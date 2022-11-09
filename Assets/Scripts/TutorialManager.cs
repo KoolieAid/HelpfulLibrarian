60b5c6dcfa01080986ae6e0366abc1f98c1f9065
LauSnchz <laurence.sanchez@benilde.edu.ph>
2022-11-09 15:38:05 +0800

    Added tutorial back button and disabled levels button

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour
+{
+    public void OnBackButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnReplayButtonClicked()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
