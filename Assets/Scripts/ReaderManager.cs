f3fee0bddfd24d4e551e9448a37b6e77cad4d72b
Alexander <alexandergregoremmanuel.sanchez@benilde.edu.ph>
2023-02-03 17:22:21 +0800

    Debug statment removed

--- Assets/Scripts/ReaderManager.cs
+++ Assets/Scripts/ReaderManager.cs
@@ -73,8 +73,6 @@ public class ReaderManager : MonoBehaviour
     private void NextReader()
     {
         // If theres no possible reader next
-
-        Debug.Log(currentCorrectAnswers.Count);
         if (currentCorrectAnswers.Count < 1 )
         {
             Debug.Log("Player won the level");
