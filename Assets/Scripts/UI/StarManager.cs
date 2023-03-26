399b3f7ac72d1f9307b98f9c7ab591c83c643365
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-03-26 21:51:44 +0800

    Update StarManager.cs

--- Assets/Scripts/UI/StarManager.cs
+++ Assets/Scripts/UI/StarManager.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class StarManager : MonoBehaviour
 {
     public GameObject[] stars = new GameObject[3];
-    
-
 
     public void SetStarsToActive(int numOfStars)
     {
-        if (numOfStars > 0) stars[0].SetActive(true);
-        if (numOfStars > 1) stars[1].SetActive(true);
-        if (numOfStars > 2) stars[2].SetActive(true);
+        for (int i = 0; i < numOfStars; i++) {
+            stars[i].SetActive(true);
+        }
     }
 
 }
