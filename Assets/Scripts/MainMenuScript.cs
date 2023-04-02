9cd3ef1d6426c7d34baed1f292ad98778d38347d
LauSnchz <laurence.sanchez@benilde.edu.ph>
2023-04-02 16:53:07 +0800

    Band aid fix
    - different bandaid fix than the trycatch

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -125,15 +125,17 @@ public class MainMenuScript : MonoBehaviour
     private void UnlockLevelButtons()
     {
         // unlock each levels
-        for (int i = 1; i < GameManager.instance.levelManager.levelsUnlocked.Count; i++)
+        for (int i = 0; i < GameManager.instance.levelManager.levelsUnlocked.Count + 1; i++)
         {
+            if(i >= 9)
+                return;
+            
             if (!levelButtons[i].interactable)
             {
                 levelButtons[i].interactable = true;
-                
+            
                 Debug.Log($"Unlocked button {levelButtons[i]}");
             }
-
         }
     }
 }
