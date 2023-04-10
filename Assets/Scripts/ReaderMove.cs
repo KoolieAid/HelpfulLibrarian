52092def02e8da55d35e96eb1e159996e9b769d7
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-04-10 12:10:43 +0800

    Main Level - Book grid spacing now scales with screen and cart / PopUps now scale with screen / Requester position now based on reference instead of values / Working on fixing bug with conformation

--- Assets/Scripts/ReaderMove.cs
+++ Assets/Scripts/ReaderMove.cs
@@ -8,10 +8,16 @@ public class ReaderMove : MonoBehaviour
     public float speed;
 
     private RectTransform readerPos;
+    private RectTransform movePosPoint;
 
     void Start()
     {
         readerPos = GetComponent<RectTransform>();
+
+        Debug.Log(gameObject.name);
+        Debug.Log(gameObject.transform.root.name);
+        //movePosPoint = gameObject.transform.root.GetComponent<RectTransform>().Find("Requester Point");
+        movePosPoint = gameObject.transform.root.transform.Find("Requester Point").GetComponent<RectTransform>();
 
         Reader.Instance.canDeduct = false;
 
@@ -21,7 +27,7 @@ public class ReaderMove : MonoBehaviour
     IEnumerator MoveReader()
     {
         // var movePos = new Vector2(280, 129);
-        var movePos = new Vector2(-286, 129);
+        var movePos = new Vector2(movePosPoint.position.x, movePosPoint.position.y);
         while (Vector2.Distance(readerPos.anchoredPosition, movePos) > 1f)
         {
             readerPos.anchoredPosition = Vector3.MoveTowards(readerPos.anchoredPosition, movePos, speed * Time.deltaTime);
