c3f571e9b0c8015c09817a61d3e4da440aee2242
Alex Sanchez <84173270+AlexanderGregorSanchez@users.noreply.github.com>
2023-04-10 13:13:47 +0800

    Requester prefab scales with screen

--- Assets/Scripts/ReaderMove.cs
+++ Assets/Scripts/ReaderMove.cs
@@ -13,11 +13,7 @@ public class ReaderMove : MonoBehaviour
     void Start()
     {
         readerPos = GetComponent<RectTransform>();
-
-        Debug.Log(gameObject.name);
-        Debug.Log(gameObject.transform.root.name);
-        //movePosPoint = gameObject.transform.root.GetComponent<RectTransform>().Find("Requester Point");
-        movePosPoint = gameObject.transform.root.transform.Find("Requester Point").GetComponent<RectTransform>();
+        movePosPoint = (RectTransform)gameObject.transform.root.transform.Find("Requester Point");
 
         Reader.Instance.canDeduct = false;
 
@@ -28,9 +24,10 @@ public class ReaderMove : MonoBehaviour
     {
         // var movePos = new Vector2(280, 129);
         var movePos = new Vector2(movePosPoint.position.x, movePosPoint.position.y);
-        while (Vector2.Distance(readerPos.anchoredPosition, movePos) > 1f)
+        while (Vector2.Distance(readerPos.position, movePos) > 1f)
         {
-            readerPos.anchoredPosition = Vector3.MoveTowards(readerPos.anchoredPosition, movePos, speed * Time.deltaTime);
+            
+            readerPos.position = Vector2.MoveTowards(readerPos.position, movePos, speed * Time.deltaTime);
             
             yield return new WaitForEndOfFrame();
         }
